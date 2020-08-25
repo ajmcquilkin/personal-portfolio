a7a5027f0d926e2029cc6b1aae6e1cc2eb331523
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-08-24 20:21:06 -0400

    Added working Nav Menu Items

---
+++ src/components/NavMenuItem/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { withRouter, NavLink, RouteComponentProps } from 'react-router-dom';
+import './NavMenuItem.scss';
+
+interface Props extends RouteComponentProps {
+  pageName: string;
+  pageLink: string;
+
+  className?: string;
+  dark?: boolean;
+}
+
+const NavMenuItem = ({
+  pageName,
+  pageLink,
+  history,
+  className = '',
+  dark = false,
+}: Props) => (
+  <nav className={`nav-menu-container${dark ? ' dark' : ' light'} ${className}`}>
+    <div className={`nav-menu-container-bar left${history.location.pathname === pageLink ? ' active' : ''}${dark ? ' dark' : ' light'}`} />
+    <NavLink exact className={dark ? 'dark' : 'light'} to={pageLink}>{pageName}</NavLink>
+    <div className={`nav-menu-container-bar right${history.location.pathname === pageLink ? ' active' : ''}${dark ? ' dark' : ' light'}`} />
+  </nav>
+);
+
+export default withRouter(NavMenuItem);
